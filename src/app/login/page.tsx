88d4d1e5890b21b043b41fe8533fbee5bde415ca
vlbui <lvbui189@gmail.com>
2026-03-31 23:18:30 +0700

    try to avoid PKCE

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `${window.location.origin}/auth/callback#`,
       },
     });
     if (error) {
