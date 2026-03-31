8841697a60358f9ba7bf84bff7bedc7a251eb65e
vlbui <lvbui189@gmail.com>
2026-03-31 23:22:05 +0700

    try to avoid PKCE

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,7 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 export function createClient() {
-  return createBrowserClient(
+  return createSupabaseClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
@@ -9,6 +9,7 @@ export function createClient() {
         flowType: "implicit",
         detectSessionInUrl: true,
         autoRefreshToken: true,
+        persistSession: true,
       },
     }
   );
